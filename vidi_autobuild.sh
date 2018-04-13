6e70687fb286a9e01e8466bf903d61eb2fdb8238
@hasherezade <hasherezade@users.noreply.github.com>
2018-04-14 01:38:50 +0200

    Update vidi_autobuild.sh

--- vidi_autobuild.sh
+++ vidi_autobuild.sh
@@ -25,21 +25,9 @@ fi
 
 mkdir ViDi
 cd ViDi
-git clone https://github.com/hasherezade/ViDi.git
+git clone --recursive https://github.com/hasherezade/ViDi.git
 echo "[+] ViDi cloned"
 mv ViDi src
-cd src
-git clone https://github.com/hasherezade/bearparser.git
-echo "[+] bearparser cloned"
-cd bearparser
-git checkout qt4
-cd ..
-git clone https://github.com/aquynh/capstone.git
-echo "[+] capstone cloned"
-echo "[+] capstone : switching to branch 'next'"
-cd capstone
-cd ..
-cd ..
 mkdir build
 echo "[+] build directory created"
 cd build
